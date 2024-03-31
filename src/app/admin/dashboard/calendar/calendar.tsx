e7e6acf626b623ce900913a96ba8831669350a73
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2024-03-31 17:40:26 +0400

    feat: integrate events API with dashboard

--- src/app/admin/dashboard/calendar/calendar.tsx
+++ src/app/admin/dashboard/calendar/calendar.tsx
@@ -1,8 +1,29 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import styles from './calendar.module.scss';
+import useSWR from 'swr';
+import { client } from '@/base/services/clients/browser-client';
+import { format, parseISO } from 'date-fns';
+
+/**
+ * Extracts the date part from a date-time string.
+ * @param {string} dateTimeString - The date-time string from which to extract the date.
+ * @returns {string} The date part in "YYYY-MM-DD" format.
+ */
+function extractDate(dateTimeString: string) {
+  try {
+    const dateObject = parseISO(dateTimeString);
+    const dateString = format(dateObject, 'yyyy-MM-dd');
+    return dateString;
+  } catch (error) {
+    console.error('Error extracting date:', error);
+    return null; // or handle the error based on your needs
+  }
+}
 
 export function DashboardCalendar() {
+  const { data = [] } = useSWR('/events', (url) => client.get(url));
+
   return (
     <div className={styles.calendarWrapper}>
       <FullCalendar
@@ -11,23 +32,10 @@ export function DashboardCalendar() {
         viewClassNames={styles.calendar}
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        events={[
-          {
-            title: 'David | Designer Interview',
-            date: '2024-03-01',
-            className: 'bg-green border-green',
-          },
-          {
-            title: 'Rose | HR Partner',
-            date: '2024-03-04 07:40',
-            className: 'bg-amber border-amber',
-          },
-          {
-            title: 'Michael | Senior Developer',
-            date: '2024-03-04 08:00',
-            className: 'bg-rose border-rose',
-          },
-        ]}
+        events={data.map((event: any) => ({
+          ...event,
+          date: extractDate(event.date),
+        }))}
         headerToolbar={{
           end: 'today prev,next',
         }}
