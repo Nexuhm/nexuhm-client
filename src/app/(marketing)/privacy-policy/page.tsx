1e5d1010f35a4808f8f6a17ecaf766278165f76c
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2024-04-15 17:03:05 +0400

    feat: add legal pages

---
+++ src/app/(marketing)/privacy-policy/page.tsx
@@ -0,0 +1,120 @@
+export default function PrivaycPolicyPage() {
+  return (
+    <div className="container prose mx-auto py-20">
+      <h1>Privacy Policy</h1>
+      <p className="font-semibold">Last updated: Feb 27, 2024</p>
+      <h2>1. Introduction</h2>
+      <p>
+        We are committed to protecting your personal information and your right
+        to privacy. If you have any questions or concerns about this privacy
+        policy, or our practices with regards to your personal information,
+        please contact us at{' '}
+        <a href="mailto:hello@nexuhm.com" className="text-blue">
+          hello@nexuhm.com
+        </a>
+        .
+      </p>
+
+      <h2>2. Information We Collect</h2>
+      <p>
+        We collect personal information that you voluntarily provide to us when
+        you register on the Website, express an interest in obtaining
+        information about us or our products and services, when you participate
+        in activities on the Website or otherwise when you contact us.
+      </p>
+      <p>
+        The personal information that we collect depends on the context of your
+        interactions with us and the Website, the choices you make and the
+        products and features you use. The personal information we collect may
+        include the following:
+      </p>
+      <ul>
+        <li>
+          Personal Information Provided by You. We collect names; phone numbers;
+          email addresses; mailing addresses; job titles; usernames; passwords;
+          contact preferences; contact or authentication data; billing
+          addresses; debit/credit card numbers and other similar information.
+        </li>
+        <li>
+          Information automatically collected. We automatically collect certain
+          information when you visit, use or navigate the Website. This
+          information does not reveal your specific identity (like your name or
+          contact information) but may include device and usage information,
+          such as your IP address, browser and device characteristics, operating
+          system, language preferences, referring URLs, device name, country,
+          location, information about how and when you use our Website and other
+          technical information. This information is primarily needed to
+          maintain the security and operation of our Website, and for our
+          internal analytics and reporting purposes.
+        </li>
+      </ul>
+
+      <h2>3. How We Use Your Information</h2>
+      <p>
+        We use personal information collected via our Website for a variety of
+        business purposes described below. We process your personal information
+        for these purposes in reliance on our legitimate business interests, in
+        order to enter into or perform a contract with you, with your consent,
+        and/or for compliance with our legal obligations. We indicate the
+        specific processing grounds we rely on next to each purpose listed
+        below.
+      </p>
+      <ul>
+        <li>To facilitate account creation and logon process.</li>
+        <li>To send you marketing and promotional communications.</li>
+        <li>To send administrative information to you.</li>
+        <li>
+          For other business purposes such as data analysis, identifying usage
+          trends, determining the effectiveness of our promotional campaigns and
+          to evaluate and improve our Website, products, services, marketing and
+          your experience.
+        </li>
+      </ul>
+
+      <h2>4. Will Your Information be Shared with Anyone?</h2>
+      <p>
+        We only share information with your consent, to comply with laws, to
+        provide you with services, to protect your rights, or to fulfill
+        business obligations.
+      </p>
+
+      <h2>5. How Do We Keep Your Information Safe?</h2>
+      <p>
+        We have implemented appropriate technical and organizational security
+        measures designed to protect the security of any personal information we
+        process. However, despite our safeguards and efforts to secure your
+        information, no electronic transmission over the Internet or information
+        storage technology can be guaranteed to be 100% secure, so we cannot
+        promise or guarantee that hackers, cybercriminals, or other unauthorized
+        third parties will not be able to defeat our security, and improperly
+        collect, access, steal, or modify your information.
+      </p>
+
+      <h2>6. What Are Your Privacy Rights?</h2>
+      <p>
+        You have certain rights under applicable data protection laws. These may
+        include the right to request access to and receive a copy of your
+        personal information, request rectification or erasure; to restrict the
+        processing of your personal information; and if applicable, to data
+        portability. In certain circumstances, you may also have the right to
+        object to the processing of your personal information. You can make such
+        a request by contacting us at{' '}
+        <a href="mailto:hello@nexuhm.com" className="text-blue">
+          hello@nexuhm.com
+        </a>
+        .
+      </p>
+
+      <h2>7. Updates to This Policy</h2>
+      <p>
+        We may update this privacy policy from time to time. The updated version
+        will be indicated by an updated "Last updated" date and the updated
+        version will be effective as soon as it is accessible. If we make
+        material changes to this privacy policy, we may notify you either by
+        prominently posting a notice of such changes or by directly sending you
+        a notification. We encourage you to review this privacy policy
+        frequently to be informed of how we are protecting your information.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/(marketing)/terms/page.tsx
@@ -0,0 +1,84 @@
+export default function TermsPage() {
+  return (
+    <div className="container prose mx-auto py-20">
+      <h1>Terms of Service</h1>
+      <p className="font-semibold">Last updated: Feb 27, 2024</p>
+
+      <h2>1. Agreement to Terms</h2>
+      <p>
+        By using our website and web application, nexuhm.com, you agree to be
+        bound by these terms. If you do not agree to these terms, do not use our
+        services.
+      </p>
+
+      <h2>2. Intellectual Property Rights</h2>
+      <p>
+        Except where otherwise noted, all content on this site, including text,
+        graphics, logos, icons, images, audio clips, digital downloads, and
+        software, is the property of Nexuhm LTD or its content
+        suppliers and protected by international copyright and trademark laws.
+        You agree not to reproduce, duplicate, copy, sell, resell or exploit any
+        portion of the Service, use of the Service, or access to the Service
+        without express written permission by us.
+      </p>
+
+      <h2>3. User Obligations</h2>
+      <p>
+        You agree to use our website only for lawful purposes and in a way that
+        does not infringe the rights of, restrict or inhibit anyone else's use
+        and enjoyment of the website. Prohibited behavior includes harassing or
+        causing distress or inconvenience to any other user, transmitting
+        obscene or offensive content or disrupting the normal flow of dialogue
+        within our website.
+      </p>
+
+      <h2>4. Disclaimers</h2>
+      <p>
+        Nexuhm LTD does not guarantee the accuracy, timeliness, performance,
+        completeness, or suitability of the information and materials found or
+        offered on this website for any particular purpose. You acknowledge that
+        such information and materials may contain inaccuracies or errors and we
+        expressly exclude liability for any such inaccuracies or errors to the
+        fullest extent permitted by law.
+      </p>
+
+      <h2>5. Modifications to the Service and Prices</h2>
+      <p>
+        Prices for our products are subject to change without notice. We reserve
+        the right at any time to modify or discontinue the Service (or any part
+        or content thereof) without notice at any time. We shall not be liable
+        to you or to any third-party for any modification, price change,
+        suspension or discontinuance of the Service.
+      </p>
+
+      <h2>6. Termination</h2>
+      <p>
+        We may terminate or suspend access to our service immediately, without
+        prior notice or liability, for any reason whatsoever, including without
+        limitation if you breach the Terms. All provisions of the Terms which by
+        their nature should survive termination shall survive termination,
+        including, without limitation, ownership provisions, warranty
+        disclaimers, indemnity, and limitations of liability.
+      </p>
+
+      <h2>7. Governing Law</h2>
+      <p>
+        These Terms shall be governed and construed in accordance with the laws
+        of UK, without regard to its conflict of law provisions. Our failure to
+        enforce any right or provision of these Terms will not be considered a
+        waiver of those rights. If any provision of these Terms is held to be
+        invalid or unenforceable by a court, the remaining provisions of these
+        Terms will remain in effect.
+      </p>
+
+      <h2>Contact Information</h2>
+      <p>
+        If you have any questions about these Terms, please contact us at{' '}
+        <a href="mailto:hello@nexuhm.com" className="text-blue">
+          hello@nexuhm.com
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
