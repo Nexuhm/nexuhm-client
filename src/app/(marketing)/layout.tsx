a1550f8813360a24bec5932ffa61d3d13a5a1395
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2024-07-23 22:07:06 +0400

    chore: Add Apollo website tracker script to marketing layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Footer } from '@/components/layouts/marketing/footer';
 import { Header } from '@/components/layouts/marketing/header';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 export default async function MarketingLayout({
   children,
@@ -11,9 +12,23 @@ export default async function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="font-poppins flex-1 flex flex-col">{children}</main>
+      <main className="flex flex-1 flex-col font-poppins">{children}</main>
       <Footer />
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
+      <Script strategy="beforeInteractive">
+        {`function initApollo() {
+          var n = Math.random().toString(36).substring(7),
+              o = document.createElement("script");
+          o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+          o.async = true;
+          o.defer = true;
+          o.onload = function() {
+            window.trackingFunctions.onLoad({ appId: "6633f3389019280438873515" });
+          };
+          document.head.appendChild(o);
+        }
+        initApollo();`}
+      </Script>
     </div>
   );
 }
